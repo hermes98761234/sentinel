9c5c95b57cf38a60fe28be5897b6dd54dd2f8a54
Bobbi-Jo Clark <hermes98761234@tutamail.com>
2026-06-04 07:58:06 +0000

    feat: API key auth service, middleware, health and usage routers

---
+++ api/routers/__init__.py
@@ -0,0 +1,6 @@
+from api.routers import health  # noqa
+from api.routers import navigator  # noqa
+from api.routers import browsing  # noqa
+from api.routers import research  # noqa
+from api.routers import scouting  # noqa
+from api.routers import usage  # noqa
